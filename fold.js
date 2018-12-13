8749ee6f1558c0ab9cb06cf5822fcaf7197dd5c1
Glitch (hello-webpage) <none>
2018-12-13 13:17:48 +0000

    🛬😂 Checkpoint ./fold.css:562062/113 ./fold.js:562062/1033 ./squares.css:562062/2

---
+++ fold.js
@@ -0,0 +1,21 @@
+let body = document.body,
+    html = document.documentElement;
+
+let height = Math.max( body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight );
+let width = Math.max( body.scrollWidth, body.offsetWidth, html.clientWidth, html.scrollWidth, html.offsetWidth );
+let gap = width/100 * 28;
+let holder = document.getElementById("holder");
+let colHeight = Math.trunc((height - gap) / 100);
+let rowLen = Math.trunc((width - gap) / 100);
+let quantity = colHeight * rowLen;
+
+console.log(height, width, gap, colHeight, rowLen)
+holder.style.width = rowLen * 100 + "px"
+
+
+for (var i = 0; i < quantity; i++) {
+  let square = document.createElement('div');
+      square.className="square";
+  
+  holder.appendChild(square);
+}
