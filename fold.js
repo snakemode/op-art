655c27ad7623eecbdd42aa5236a4af3b66e0e700
Glitch (hello-webpage) <none>
2018-12-13 15:03:06 +0000

    🙍🍐 Checkpoint ./fold.css:562062/244 ./fold.js:562062/11

--- fold.js
+++ fold.js
@@ -6,9 +6,9 @@ let squares = document.getElementsByClassName("square");
 
 let width = Math.max( art.scrollWidth, art.offsetWidth, art.clientWidth, art.scrollWidth, art.offsetWidth );
 
-for (var i = 1; i < 7; i++) {
+for (var i = 1; i < 10; i++) {
   console.log(squares[i]);
-  squares[i].style.width = 40 - i*i + "px";
+  squares[i].style.width = 40 - i*(i*0.5) + "px";
 }
 
 
