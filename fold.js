808df0a1fecc9907673388bdb5b9203bc1c2af00
Glitch (hello-webpage) <none>
2018-12-13 14:31:22 +0000

    😭👩‍❤️‍💋‍👩 Checkpoint ./fold.css:562062/119 ./fold.js:562062/294

--- fold.js
+++ fold.js
@@ -2,15 +2,13 @@ let body = document.body,
     html = document.documentElement;
 
 let art = document.getElementById("art");
+let squares = document.getElementsByClassName("square");
 
 let width = Math.max( art.scrollWidth, art.offsetWidth, art.clientWidth, art.scrollWidth, art.offsetWidth );
-let quantity = 500;
 
-let starter = 50;
-let variance = 10;
-
-for (var i = 0; i < 38; i++) {
-  
+for (var i = 1; i < 40; i++) {
+  console.log(squares[i]);
+  squares[i].style.width = i * i + "px";
 }
 
 
