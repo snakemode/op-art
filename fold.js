878946753ac06b5791e997bb2b598d559fe8e2cc
Glitch (hello-webpage) <none>
2018-12-13 14:10:20 +0000

    🛀👹 Checkpoint ./fold.js:562062/195 ./fold.html:562062/26547 ./fold.css:562062/21

--- fold.js
+++ fold.js
@@ -6,15 +6,11 @@ let art = document.getElementById("art");
 let width = Math.max( art.scrollWidth, art.offsetWidth, art.clientWidth, art.scrollWidth, art.offsetWidth );
 let quantity = 500;
 
-let starter = 3;
+let starter = 50;
+let variance = 10;
 
-console.log(remainder);
-
-for (var i = 0; i < quantity; i++) {
-  let square = document.createElement('div');
-      square.className="square";
+for (var i = 0; i < 38; i++) {
   
-  art.appendChild(square);
 }
 
 
