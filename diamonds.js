ca681db8f71c42ad60713793e43306a0775c84fa
Glitch (hello-webpage) <none>
2020-01-21 14:17:32 +0000

    👨🎅 Checkpoint ./diamonds.js:562062/960

--- diamonds.js
+++ diamonds.js
@@ -1,6 +1,7 @@
-let ably = new Ably.Realtime('UeZ7NA.1_lKNA:4Twl7lf-0ozS09kQ');
-let channelName = '[product:ably-tfl/tube]tube:northern:940GZZLUEUS:arrivals';
+let ably = new Ably.Realtime('2L2RQA.8DEPlw:Oc37iQaXFFdvT-Zx');
+let channelName = '[product:ably-tfl/tube]tube:disruptions';
 let channel = ably.channels.get(channelName);
 channel.subscribe((msg) => {
- console.log(msg);
+ console.log('hello: ' + msg.data);
+
 });
