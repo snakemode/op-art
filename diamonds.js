9a5759949b7249903f93a14b8fae2c4cc67559d7
Glitch (hello-webpage) <none>
2020-01-21 14:40:16 +0000

    💶🎰 Checkpoint ./diamonds.js:562062/50

--- diamonds.js
+++ diamonds.js
@@ -1,5 +1,5 @@
 let ably = new Ably.Realtime('2L2RQA.8DEPlw:Oc37iQaXFFdvT-Zx');
-let channelName = '[product:ably-tfl/tube]tube:northern:940GZZLUEUS:arrivals';
+let channelName = '[product:ably-tfl/tube]tube:disruptions';
 let channel = ably.channels.get(channelName);
 channel.subscribe((msg) => {
   let data = JSON.stringify(msg.data);
