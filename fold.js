f569da46c14d63add51fe90705c9afdf28d0993c
Glitch (hello-webpage) <none>
2020-01-21 16:09:20 +0000

    🍎📫 Checkpoint ./fold.js:562062/923

--- fold.js
+++ fold.js
@@ -1,8 +1,24 @@
 let ably = new Ably.Realtime('2L2RQA.NRr7ZQ:DddGQeHfnaZsHCv7');
-let channel = ably.channels.get('[product:ably-openweathermap/weather]weather:2643741');
+let channelName = '[product:ably-openweathermap/weather]weather:2643741';
+let channel = ably.channels.get(channelName);
 channel.subscribe((msg) => {
     console.log(msg)
 });
+
+ function historyOpenWeather() {
+    channelName.attach(function(err) {
+      channelName.history({ untilAttach: true, limit: 1 }, function(err, resultPage) {
+        if(err){
+            console.log(err)
+            return
+        }
+        let recentMessage = resultPage.items[0];
+        if(recentMessage) {
+          $resultWeather.text((recentMessage.data.main.temp - 273.15 ).toFixed(2)+ '°C with ' + recentMessage.data.weather[0].description);
+        }
+      });
+    });
+  }
 
 let body = document.body,
     html = document.documentElement;
