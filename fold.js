663e9afa916b8c505ae70e446d59f0b2be19562c
Glitch (hello-webpage) <none>
2020-01-21 17:24:10 +0000

    🙏🚶 Checkpoint ./fold.js:562062/426 ./fold.css:562062/107 ./fold.html:562062/882

--- fold.js
+++ fold.js
@@ -1,6 +1,6 @@
 let ably = new Ably.Realtime('2L2RQA.NRr7ZQ:DddGQeHfnaZsHCv7');
 
-let channelWeather = ably.channels.get('[product:ably-openweathermap/weather]weather:' + id);
+let channelWeather = ably.channels.get('[product:ably-openweathermap/weather]weather:2643741');
 let newTemperature, weatherDesc;
 historyOpenWeather();
 //subscribing to updates in the weather data
@@ -18,24 +18,9 @@ function historyOpenWeather() {
       }
       let recentMessage = resultPage.items[0];
       if(recentMessage) {
-        console.log(recentMessage.data.main.temp - 273.15 ).toFixed(2)+ '°C with ' + recentMessage.data.weather[0].description);
+        console.log((recentMessage.data.main.temp - 273.15 ).toFixed(2)+ '°C');
       }
     });
   });
 }
-
-let body = document.body,
-    html = document.documentElement;
-
-let art = document.getElementById("art");
-let squares = document.getElementsByClassName("square");
-
-let width = Math.max( art.scrollWidth, art.offsetWidth, art.clientWidth, art.scrollWidth, art.offsetWidth );
-
-for (var i = 1; i < 10; i++) {
-  squares[i].style.width = 40 - i*(i*0.5) + "px";
-}
-
-
-
        
