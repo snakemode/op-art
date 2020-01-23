036d27b3821671797ac9d2574626b324c077eb5c
Glitch (hello-webpage) <none>
2020-01-23 13:57:49 +0000

    🗺🎲 Checkpoint ./diamonds.js:562062/2994

--- diamonds.js
+++ diamonds.js
@@ -1,7 +1,47 @@
 let ably = new Ably.Realtime('2L2RQA.8DEPlw:Oc37iQaXFFdvT-Zx');
-let channelName = '[product:ably-tfl/tube]tube:northern:940GZZLUEUS:arrivals';
-let channel = ably.channels.get(channelName);
-channel.subscribe((msg) => {
-  let data = JSON.stringify(msg.data);
-  console.log(data);
-});
+
+function subscribeTfl(name) { 
+    channelTfl = ably.channels.get('[product:ably-tfl/tube]tube:northern:940GZZLUEUS:arrivals');
+    historyTfl();
+    channelTfl.subscribe(function(msg) {
+      updateTfl(msg.data);
+    });
+  }
+
+  function updateTfl(arrivals) {
+    arrivals = arrivals.reverse();
+    let newBodyTfl = document.createElement('tbody');
+    arrivals.forEach((arrival) => {
+      let arrivalTime = new Date(arrival.ExpectedArrival).toLocaleTimeString();
+      let tmpTableRow = newBodyTfl.insertRow(0);
+      tmpTableRow.insertCell(0).innerHTML = arrivalTime;
+      tmpTableRow.insertCell(1).innerHTML = arrival.Towards;
+      tmpTableRow.insertCell(2).innerHTML = arrival.PlatformName;
+      tmpTableRow.insertCell(3).innerHTML = arrival.LineName;
+    });
+    for(let i=0; i < arrivals.length; i++) {
+      let arrivalTime = new Date(arrivals[arrivals.length - i - 1].ExpectedArrival).toLocaleTimeString();
+      if(tableBodyTfl.rows[i] != undefined && newBodyTfl.rows[i] != undefined){
+        if(tableBodyTfl.rows[i].cells[0].innerHTML != arrivalTime) {
+          newBodyTfl.rows[i].classList.add("c-flashbox__positive-flash");
+        }
+      }
+    }
+
+    tableBodyTfl.parentNode.replaceChild(newBodyTfl, tableBodyTfl);
+    tableBodyTfl = newBodyTfl;
+  }
+  function historyTfl() {
+    channelTfl.attach(function(err) {
+      channelTfl.history({ untilAttach: true, limit: 1 }, function(err, resultPage) {
+        if(err){
+            console.log(err)
+            return
+        }
+        let recentMessage = resultPage.items[0];
+        if(recentMessage) {
+          updateTfl(recentMessage.data);
+        }
+      });
+    });
+  }
