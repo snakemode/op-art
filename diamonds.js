3443ae2146c1c46dfd655071d58b4fcb2f0e6c5f
Glitch (hello-webpage) <none>
2020-01-23 14:08:50 +0000

    🌦🏔 Checkpoint ./diamonds.js:562062/1257

--- diamonds.js
+++ diamonds.js
@@ -1,35 +1,20 @@
 let ably = new Ably.Realtime('2L2RQA.8DEPlw:Oc37iQaXFFdvT-Zx');
+let channelTfl = ably.channels.get('[product:ably-tfl/tube]tube:northern:940GZZLUEUS:arrivals');
 
-function subscribeTfl(name) { 
-    channelTfl = ably.channels.get('[product:ably-tfl/tube]tube:northern:940GZZLUEUS:arrivals');
+function subscribeTfl(name) {
     historyTfl();
     channelTfl.subscribe(function(msg) {
       updateTfl(msg.data);
+      console.log('live: ', msg.data)
     });
   }
 
   function updateTfl(arrivals) {
     arrivals = arrivals.reverse();
-    let newBodyTfl = document.createElement('tbody');
+ 
     arrivals.forEach((arrival) => {
       let arrivalTime = new Date(arrival.ExpectedArrival).toLocaleTimeString();
-      let tmpTableRow = newBodyTfl.insertRow(0);
-      tmpTableRow.insertCell(0).innerHTML = arrivalTime;
-      tmpTableRow.insertCell(1).innerHTML = arrival.Towards;
-      tmpTableRow.insertCell(2).innerHTML = arrival.PlatformName;
-      tmpTableRow.insertCell(3).innerHTML = arrival.LineName;
     });
-    for(let i=0; i < arrivals.length; i++) {
-      let arrivalTime = new Date(arrivals[arrivals.length - i - 1].ExpectedArrival).toLocaleTimeString();
-      if(tableBodyTfl.rows[i] != undefined && newBodyTfl.rows[i] != undefined){
-        if(tableBodyTfl.rows[i].cells[0].innerHTML != arrivalTime) {
-          newBodyTfl.rows[i].classList.add("c-flashbox__positive-flash");
-        }
-      }
-    }
-
-    tableBodyTfl.parentNode.replaceChild(newBodyTfl, tableBodyTfl);
-    tableBodyTfl = newBodyTfl;
   }
   function historyTfl() {
     channelTfl.attach(function(err) {
@@ -41,6 +26,7 @@ function subscribeTfl(name) { 
         let recentMessage = resultPage.items[0];
         if(recentMessage) {
           updateTfl(recentMessage.data);
+          console.log('recent: ', recentMessage.data);
         }
       });
     });
