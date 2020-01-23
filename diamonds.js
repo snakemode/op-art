2e65e4220da5b19613fe31114e5dc15c7e51b440
Glitch (hello-webpage) <none>
2020-01-23 17:23:23 +0000

    🏉🛋 Checkpoint ./style.css:562062/1293 ./diamonds.js:562062/90:7701387/3163 ./index.html:562062/16 ./diamonds.html:562062/790

--- diamonds.js
+++ diamonds.js
@@ -1,55 +1,56 @@
 let ably = new Ably.Realtime('2L2RQA.8DEPlw:Oc37iQaXFFdvT-Zx');
-let northern = '[product:ably-tfl/tube]tube:northern:940GZZLUKSX:arrivals';
-let victoria = '[product:ably-tfl/tube]tube:victoria:940GZZLUKSX:arrivals';
-let metropolitan = '[product:ably-tfl/tube]tube:metropolitan:940GZZLUKSX:arrivals';
-let piccadilly = '[product:ably-tfl/tube]tube:piccadilly:940GZZLUKSX:arrivals';
-let hammersmith = '[product:ably-tfl/tube]tube:hammersmith-city:940GZZLUKSX:arrivals';
-let circle = '[product:ably-tfl/tube]tube:circle:940GZZLUKSX:arrivals';
-
 let trains = {};
 let ordered = {};
 
-
 function updateTfl(arrivals) {
   arrivals = arrivals.reverse();
-
   arrivals.forEach((arrival) => {
     let arrivalTime = new Date(arrival.ExpectedArrival).toLocaleTimeString();
   });
 }
 
-function historyTfl(channel, resolve, reject) {
-
+function onSubscriptionData(i, i2) {
+ console.log("Some subscription data appeared"); 
 }
 
-function subscribeTfl(channel) {  
-    return new Promise((resolve, reject) => { 
-      
-      let channelTfl = ably.channels.get(channel);
-     
-      /*channelTfl.subscribe(function(msg) {
-        console.log(msg.data);
-      });*/
-      
-      channelTfl.attach(function(err) {
-        channelTfl.history({ untilAttach: true, limit: 1 }, function(err, resultPage) {
-          if(err){
-              reject(err);
-              return
-          }
-          
-          let recentMessage = resultPage.items[0];
-          
-          if(recentMessage) {            
-            resolve(resultPage.items[0].data);
-          } else { 
-            resolve([]);
-          }          
-        });
+function subscribeTfl(channelName, onSubscriptionData) {
+  const channel = `[product:ably-tfl/tube]tube:${channelName}:940GZZLUKSX:arrivals`;
+
+  return new Promise((resolve, reject) => { 
+    let channelTfl = ably.channels.get(channel);
+
+    channelTfl.attach(function(err) {
+      channelTfl.history({ untilAttach: true, limit: 1 }, function(err, resultPage) {
+        console.log("History retrieved for " + channelName); 
+        
+        if(err) {
+            reject(err);
+            return;
+        }
+
+        let recentMessage = resultPage.items[0];
+
+        if(recentMessage) {            
+          resolve(resultPage.items[0].data);
+        } else { 
+          resolve([]);
+        }          
       });
       
-      console.log("Subscribed to " + channel);      
-    }); 
+      // channelTfl.subscribe(onSubscriptionData);
+    });
+
+    console.log("Subscribing to " + channel);      
+  }); 
+}
+
+function sortByArrivalTime(i1, i2) {
+    if(i1.ExpectedArrival < i2.ExpectedArrival) {
+      return -1;
+    } else if(i1.ExpectedArrival > i2.ExpectedArrival) {
+      return 1;
+    }
+    return 0;
 }
 
 async function subscribeAll() {
@@ -57,36 +58,22 @@ async function subscribeAll() {
   
   let allTrains = [];
   
-  const fetchAllItems = [
-    subscribeTfl(northern),
-    subscribeTfl(victoria),
-    subscribeTfl(metropolitan),
-    subscribeTfl(piccadilly),
-    subscribeTfl(hammersmith),
-    subscribeTfl(circle)    
+  const subscriptionPromises = [
+    subscribeTfl("northern"),
+    subscribeTfl("victoria"),
+    subscribeTfl("metropolitan"),
+    subscribeTfl("piccadilly"),
+    subscribeTfl("hammersmith-city"),
+    subscribeTfl("circle")    
   ];
   
-  const items = await Promise.all(fetchAllItems);
+  const items = await Promise.all(subscriptionPromises);
+  allTrains.push(...items.flat());
   
-  /*allTrains.push(... await subscribeTfl(northern));
-  allTrains.push(... await subscribeTfl(victoria));
-  allTrains.push(... await subscribeTfl(metropolitan));
-  allTrains.push(... await subscribeTfl(piccadilly));
-  allTrains.push(... await subscribeTfl(hammersmith));
-  allTrains.push(... await subscribeTfl(circle));*/
+  const orderedTrains = allTrains.sort(sortByArrivalTime);
   
-  console.log("Fetched initial state.")
-  
-  const orderedTrains = allTrains.sort((i1, i2) => {
-    if(i1.ExpectedArrival < i2.ExpectedArrival) {
-      return -1;
-    } else if(i1.ExpectedArrival > i2.ExpectedArrival) {
-      return 1;
-    }
-    return 0;
+  orderedTrains.forEach(() => {
   });
-  
-  
   console.log(JSON.stringify(orderedTrains));
 }
 
